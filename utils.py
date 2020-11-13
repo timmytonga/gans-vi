56edd60ff96d384b362116234e35bc9e0da4488d
Konstantina <33751014+showerDevice@users.noreply.github.com>
2020-11-14 00:32:08 +0200

    Updated utils

--- utils.py
+++ utils.py
@@ -27,3 +27,21 @@ def make_noise(num_noise, z_dim=128, device='cpu'):
     """ Returns a tensor of size (num_noise, z_dim)"""
     return torch.randn(num_noise, z_dim, device=device)
 
+def weights_init(m):
+	if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+		torch.nn.init.normal_(m.weight, 0.0, 0.02)
+	if isinstance(m, nn.BatchNorm2d):
+		torch.nn.init.normal_(m.weight, 0.0, 0.02)
+		torch.nn.init.constant_(m.bias, 0)
+        
+def weights_init_zero(m):
+  if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+      torch.nn.init.constant_(m.weight, 0.0)
+  if isinstance(m, nn.BatchNorm2d):
+      torch.nn.init.constant_(m.weight, 0.0)
+      torch.nn.init.constant_(m.bias, 0.0)
+        
+def get_cur_timestamp():
+	tz_NY = pytz.timezone('America/New_York')
+	dateTimeObj = datetime.now(tz_NY)
+	return dateTimeObj.strftime("[%d-%b-%Y(%H:%M)]")
