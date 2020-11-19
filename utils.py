a50f5d6b518b6916a75852ad891e4f3714276cd8
Thien <thienhn4@uci.edu>
2020-11-19 16:00:00 -0500

    unnorm finally fixed?

--- utils.py
+++ utils.py
@@ -5,20 +5,19 @@ from torchvision.utils import make_grid
 from datetime import datetime     # to get time stamp for checkpts
 import pytz
 
-
 def show_tensor_images(image_tensor, num_images=25, unnorm_mean = (0,0,0), unnorm_var = (1,1,1)):
-    """
-    Function for visualizing images: Given a tensor of images, number of images, and
-    size per image, plots and prints the images in an uniform grid.
+	"""
+	Function for visualizing images: Given a tensor of images, number of images, and
+	size per image, plots and prints the images in an uniform grid.
 
-    """
-    var = torch.tensor(unnorm_var)
-    mean = torch.tensor(unnorm_mean)
-    image_tensor = image_tensor.mul_(var).add_(mean) 
-    image_unflat = image_tensor.detach().cpu()
-    image_grid = make_grid(image_unflat[:num_images], nrow=5)
-    plt.imshow(image_grid.permute(1, 2, 0).squeeze())
-    plt.show()
+	"""
+	var = torch.tensor(unnorm_var).view(1, 3, 1, 1)
+	mean = torch.tensor(unnorm_mean).view(1, 3, 1, 1)
+	image_unflat = image_tensor.detach().cpu()
+	image_unflat = image_unflat.mul_(var).add_(mean) 
+	image_grid = make_grid(image_unflat[:num_images], nrow=5)
+	plt.imshow(image_grid.permute(1, 2, 0).squeeze())
+	plt.show()
 
 
 def make_noise(num_noise, z_dim=128, device='cpu'):
