af7716e93e9d57d999a04a5d8343127526c695ce
Thien <thienhn4@uci.edu>
2020-11-19 15:14:53 -0500

    fixed show tensor image

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ from datetime import datetime     # to get time stamp for checkpts
 import pytz
 
 
-def show_tensor_images(image_tensor, num_images=25):
+def show_tensor_images(image_tensor, num_images=25, normalizing_const = (0, 1)):
     """
     Function for visualizing images: Given a tensor of images, number of images, and
     size per image, plots and prints the images in an uniform grid.
 
     """
-    image_tensor = (image_tensor + 1) / 2
+    image_tensor = image_tensor * normalizing_const[1] - normalizing_const[0]
     image_unflat = image_tensor.detach().cpu()
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
