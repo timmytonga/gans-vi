ca5b23463aaa184a5f607f6e756cc1201fecd43c
Thien <thienhn4@uci.edu>
2020-11-19 15:28:54 -0500

    unnorm show

--- utils.py
+++ utils.py
@@ -6,13 +6,13 @@ from datetime import datetime     # to get time stamp for checkpts
 import pytz
 
 
-def show_tensor_images(image_tensor, num_images=25, normalizing_const = (0, 1)):
+def show_tensor_images(image_tensor, num_images=25, unnorm_mean = (0,0,0), unnorm_var = (1,1,1)):
     """
     Function for visualizing images: Given a tensor of images, number of images, and
     size per image, plots and prints the images in an uniform grid.
 
     """
-    image_tensor = image_tensor * normalizing_const[1] - normalizing_const[0]
+    image_tensor = image_tensor.mul(unnorm_var).add(unnorm_mean) 
     image_unflat = image_tensor.detach().cpu()
     image_grid = make_grid(image_unflat[:num_images], nrow=5)
     plt.imshow(image_grid.permute(1, 2, 0).squeeze())
