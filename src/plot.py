ce31658a134123160b0a47a67d6b456d54348e45
Eric Xu <ericxuz@outlook.com>
2021-07-20 11:32:51 -0700

    added plotter

--- src/plot.py
+++ src/plot.py
@@ -5,8 +5,8 @@ api = wandb.Api()
 
 svrg_run_paths = {"adapegsvrg_optimistic": api.run("optimproject/optimproj/3k93dg4d"),
                   "adapegsvrg_nooptimistic": api.run("optimproject/optimproj/2vag1nlz"),
-                  "optimistic_svrg": api.run("optimproject/optimproj/vw1rtpeg"),}
-                  #"extraadam_svrg": api.run("optimproject/optimproj/j0o8oyqs")}
+                  "optimistic_svrg": api.run("optimproject/optimproj/vw1rtpeg"),
+                  "extraadam_svrg": api.run("optimproject/optimproj/j0o8oyqs")}
 
 fig, axs = plt.subplots(2, sharex=True)
 x_data = list(range(0, 12))
@@ -17,7 +17,7 @@ for run_name, run in svrg_run_paths.items():
         dis_data = []
         if run_name == "adapegsvrg_optimistic":
             for i, row in run.history().iterrows():
-                if i % 20 == 0:
+                if (i+1) % 20 == 0:
                     gen_data.append(row["GEN_VARIANCE"])
                     dis_data.append(row["DIS_VARIANCE"])
         else:
@@ -26,6 +26,8 @@ for run_name, run in svrg_run_paths.items():
                     gen_data.append(row["GEN_VARIANCE"])
                 if str(row["DIS_VARIANCE"]) != "nan":
                     dis_data.append(row["DIS_VARIANCE"])
+
+        print(len(gen_data))
 
         axs[0].plot(x_data, gen_data[:12], label=run_name)
         axs[1].plot(x_data, dis_data[:12], label=run_name)
